ca84c9913e248df8c9df918b33ca59c1ee8edaa0
Sai Krishna Nandikonda <saikrishna.nandikonda@sjsu.edu>
2020-04-20 21:35:57 -0700

    Add Product css change

--- frontend/src/components/product-module/addProduct.jsx
+++ frontend/src/components/product-module/addProduct.jsx
@@ -115,20 +115,28 @@ export default class AddProduct extends Component {
             {/* </div> */}
             <div class="content">
               {/* <div class="row"> */}
-                <div className="row" style={{fontWeight:"bold",fontSize:"20",margin:10,textAlign:"center"}}>
-                  {sellerProduct.productName}
-                </div>
-                <div className="row" style={{margin:10,textAlign:"center"}}>
-                  {sellerProduct.productDesc}
-                </div>
-                <div className="row" style={{margin:10,textAlign:"center"}}>
-                  ${sellerProduct.price}
-                </div>
+              <div
+                className="row"
+                style={{
+                  fontWeight: "bold",
+                  fontSize: "20",
+                  margin: 10,
+                  textAlign: "center",
+                }}
+              >
+                {sellerProduct.productName}
+              </div>
+              <div className="row" style={{ margin: 10, textAlign: "center" }}>
+                {sellerProduct.productDesc}
+              </div>
+              <div className="row" style={{ margin: 10, textAlign: "center" }}>
+                ${sellerProduct.price}
+              </div>
               {/* </div> */}
             </div>
-            <hr style={{height:"2px",backgroundColor:"gray"}}></hr>
+            <hr style={{ height: "2px", backgroundColor: "gray" }}></hr>
             <div class="extra content">
-              <a href="#" style={{fontSize:"15"}}>
+              <a href="#" style={{ fontSize: "15" }}>
                 {/* <i aria-hidden="true" class="user icon"></i> */}
                 View Product
               </a>
@@ -139,107 +147,112 @@ export default class AddProduct extends Component {
     });
     return (
       <div>
-        <div className="container">
-          <div className="row">
-            <img
-              src={require("../../utils/logo.jpg")}
-              style={{ float: "center", height: "100px", width: "200px" }}
-              alt="hs"
-            />
-
-            <div className="panel panel-primary">
-              <div className="panel-body">
-                <Form>
-                  <div className="form-group">
-                    <h3>Add Product</h3>
+        <div className="auth-wrapper">
+          <div className="auth-inner">
+            <div className="container">
+              <div className="row2">
+                <div className="panel panel-primary">
+                  <div className="panel-body">
+                    <div className="form-group">
+                      <h3 style={{ float: "center" }}>Add Product</h3>
+                    </div>
+                    <Form>
+                      <div className="form-group">
+                        <strong>Product Name</strong>
+                        <input
+                          name="productName"
+                          type="text"
+                          maxlength="50"
+                          className="form-control"
+                          onChange={(e) =>
+                            this.setState({
+                              productName: e.target.value,
+                            })
+                          }
+                        />
+                      </div>
+                      <div className="form-group">
+                        <strong>Product Description</strong>
+                        <input
+                          name="productDesc"
+                          type="text"
+                          maxlength="100"
+                          className="form-control"
+                          onChange={(e) =>
+                            this.setState({
+                              productDesc: e.target.value,
+                            })
+                          }
+                        />
+                      </div>
+                      <div className="form-group">
+                        <strong>Price</strong>
+                        <input
+                          name="price"
+                          type="text"
+                          maxlength="100"
+                          className="form-control"
+                          onChange={(e) =>
+                            this.setState({
+                              price: e.target.value,
+                            })
+                          }
+                        />
+                      </div>
+                      <div className="form-group">
+                        <strong>Category</strong>
+                        <select
+                          placeholder="Select Category"
+                          defaultValue=""
+                          class="form-control"
+                          name="category"
+                          onChange={(e) =>
+                            this.setState({
+                              category: e.target.value,
+                            })
+                          }
+                        >
+                          <option value="Electronics">Electronics</option>
+                          <option value="Kitchen">Kitchen</option>
+                          <option value="Clothing">Clothing</option>
+                          <option value="Furniture">Furniture</option>
+                          <option value="Rentals">Rentals</option>
+                        </select>
+                      </div>
+                      <div
+                        className="form-group"
+                        style={{ paddingTop: "12px" }}
+                      >
+                        <Button
+                          variant="warning"
+                          size="lg"
+                          block
+                          onClick={(event) => this.addProduct(event)}
+                        >
+                          Add Product
+                        </Button>
+                      </div>
+                      <p className="form-group">
+                        By clicking here you are adding a product to{" "}
+                        <a href="#">Amazon</a> so that customers can buy
+                      </p>
+                      <div
+                        className="form-group"
+                        style={{ paddingTop: "12px" }}
+                      >
+                        <Button
+                          variant="warning"
+                          size="lg"
+                          block
+                          onClick={(event) => this.viewAllSellerProducts(event)}
+                        >
+                          View All Products
+                        </Button>
+                      </div>
+                    </Form>
                   </div>
-                  <div className="form-group">
-                    <strong>Product Name</strong>
-                    <input
-                      name="productName"
-                      type="text"
-                      maxlength="50"
-                      className="form-control"
-                      onChange={(e) =>
-                        this.setState({
-                          productName: e.target.value,
-                        })
-                      }
-                    />
-                  </div>
-                  <div className="form-group">
-                    <strong>Product Description</strong>
-                    <input
-                      name="productDesc"
-                      type="text"
-                      maxlength="100"
-                      className="form-control"
-                      onChange={(e) =>
-                        this.setState({
-                          productDesc: e.target.value,
-                        })
-                      }
-                    />
-                  </div>
-                  <div className="form-group">
-                    <strong>Price</strong>
-                    <input
-                      name="price"
-                      type="text"
-                      maxlength="100"
-                      className="form-control"
-                      onChange={(e) =>
-                        this.setState({
-                          price: e.target.value,
-                        })
-                      }
-                    />
-                  </div>
-                  <div className="form-group">
-                    <strong>Category</strong>
-                    <select
-                      placeholder="Select Category"
-                      defaultValue=""
-                      class="form-control"
-                      name="category"
-                      onChange={(e) =>
-                        this.setState({
-                          category: e.target.value,
-                        })
-                      }
-                    >
-                      <option value="Electronics">Electronics</option>
-                      <option value="Kitchen">Kitchen</option>
-                      <option value="Clothing">Clothing</option>
-                      <option value="Furniture">Furniture</option>
-                      <option value="Rentals">Rentals</option>
-                    </select>
-                  </div>
-                  <div className="form-group" style={{ paddingTop: "12px" }}>
-                    <Button
-                      variant="warning"
-                      size="lg"
-                      block
-                      onClick={(event) => this.addProduct(event)}
-                    >
-                      Add Product
-                    </Button>
-                  </div>
-                  <p className="form-group">
-                    By clicking here you are adding a product to{" "}
-                    <a href="#">Amazon</a> so that customers can buy
-                  </p>
-                  <div className="form-group" style={{ paddingTop: "12px" }}>
-                    <Button
-                      variant="warning"
-                      size="lg"
-                      block
-                      onClick={(event) => this.viewAllSellerProducts(event)}
-                    >
-                      View All Products
-                    </Button>
-                  </div>
-                </Form>
+                </div>
+                {sellerProducts}
               </div>
             </div>
             <div className="row">{sellerProducts}</div>
