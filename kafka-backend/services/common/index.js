615642204ccdb54dbf56a7cee102f3edd3f5376e
KailashNath <kailashnath.tallapragada@sjsu.edu>
2020-05-08 04:42:59 -0700

    msg route issue and select card issue fix

--- kafka-backend/services/common/index.js
+++ kafka-backend/services/common/index.js
@@ -1,19 +1,20 @@
 "use strict";
 const { search } = require("./search");
 const { productSearchBDSK } = require("./productSearchBDSK");
-const {dummy } = require("./dummy");
+const { dummy } = require("./dummy");
 
 let handle_request = (msg, callback) => {
-  switch (msg.route) {
-    case "search":
-      search(msg, callback);
-      break;
-    case "productSearchBDSK":
-      productSearchBDSK(msg, callback);
-    case "createPro":
-      dummy(msg, callback);
-      break;
-    
+  if (msg) {
+    switch (msg.route) {
+      case "search":
+        search(msg, callback);
+        break;
+      case "productSearchBDSK":
+        productSearchBDSK(msg, callback);
+      case "createPro":
+        dummy(msg, callback);
+        break;
+    }
   }
 };
 
--- kafka-backend/services/order/index.js
+++ kafka-backend/services/order/index.js
@@ -1,9 +1,8 @@
 "use strict";
 const { getOrders } = require("./getOrders");
-const {cancelOrders} = require ("./cancelOrders")
-const {getCancelledOrders} = require ("./getCancelledOrders")
-const {getOpenOrders} = require ("./getOpenOrders")
-
+const {cancelOrders} = require("./cancelOrders");
+const {getCancelledOrders} = require("./getCancelledOrders");
+const {getOpenOrders} = require("./getOpenOrders");
 
 let handle_request = (msg, callback) => {
     // console.log('route is::', msg.route)
@@ -11,16 +10,17 @@ let handle_request = (msg, callback) => {
   switch (msg.route) {
     case "getAllOrders":
         getOrders(msg, callback);
-      break;
-    case "cancelOrder":
-        cancelOrders(msg,callback)
         break;
-    case "getCancelledOrders":
-        getCancelledOrders(msg,callback)
+      case "cancelOrder":
+        cancelOrders(msg, callback);
         break;
-    case "getOpenOrders":
-      getOpenOrders(msg,callback)
+      case "getCancelledOrders":
+        getCancelledOrders(msg, callback);
         break;
+      case "getOpenOrders":
+        getOpenOrders(msg, callback);
+        break;
+    }
   }
 };
 
